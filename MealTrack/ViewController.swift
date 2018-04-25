e80a569ae6e486c719c1b039febe5bfc0ae4e7ae
Faisal Manzer <faisal_manzer@yahoo.in>
2018-04-25 19:10:40 +0530

    Just Created the project, removed didReceiveMemoryWarning() from ViewController

---
+++ MealTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTrack
+//
+//  Created by Faisal Manzer on 25/04/18.
+//  Copyright © 2018 Faisal Manzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
