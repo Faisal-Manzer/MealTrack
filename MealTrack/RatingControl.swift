0a0f13848dba20e40aa2b48c51be95313c8e5968
Faisal Manzer <faisal_manzer@yahoo.in>
2018-04-26 23:30:44 +0530

    Added the controllers, and array of buttons

--- MealTrack/RatingControl.swift
+++ MealTrack/RatingControl.swift
@@ -12,7 +12,11 @@ import UIKit
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
             setupButtons()
@@ -40,11 +44,29 @@ import UIKit
     
     //MARK: Private Methods
     private func setupButtons(){
+        
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
         for _ in 0..<starCount {
             
             // Create the button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // add constrains
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -57,12 +79,34 @@ import UIKit
             // add button to the stack
             addArrangedSubview(button)
             
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+            
         }
+        
+        updateButtonSelectionStates()
     }
     
     //MARK: Button Action
     @objc func ratingButtonTapped(button: UIButton){
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
+    }
+    
+    func updateButtonSelectionStates() {
+        // ddjdd
     }
     
 }
